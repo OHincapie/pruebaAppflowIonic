02771fd81e4f3136b2767282ec4031cad1942d30
Oscar David Hincapie <oscar.hincapie@fitideas.co>
2022-11-02 15:44:21 -0500

    Se actualiza sh

--- ios/App/appcenter-post-clone.sh
+++ ios/App/appcenter-post-clone.sh
@@ -1,20 +1,10 @@
 #!/usr/bin/env bash
-
-# fail if any command fails
-set -e
-# debug log
-set -x
-
-# Required nodeJS version
-NODE_VERSION=16.15.0
-
-#!/usr/bin/env bash
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 sudo npm install yarn -g
 sudo npm install -g ionic
-cd ../..
+cd ../../
 npm i
 ionic build
 npx cap sync
